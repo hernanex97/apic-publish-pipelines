2a3a1e65504d8271f3d427dc08d0ff9d9c7ff7e0
Hernan Exposito <expositohernan@ibm.com>
2024-02-21 16:30:10 -0300

    curdir

--- scripts/apic_platform_publish_to_catalog.py
+++ scripts/apic_platform_publish_to_catalog.py
@@ -18,9 +18,7 @@ def get_api_name_from_product(env_local_target_dir, product_file_name):
     
     var_apilist = []
     try:
-        print("OS LIST DIR IN: get_api_name_from_product ")
-        print(os.listdir())
-        os.listdir()
+        print(os.curdir())
         # with open(env_local_target_dir + "/" + product_file_name) as f:
         with open("/source/config/" + product_file_name) as f:
             print("printing F in open")
