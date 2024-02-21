72e17442521a52b81f2a9fe3295f039f65b8da8f
Hernan Exposito <expositohernan@ibm.com>
2024-02-21 12:38:27 -0300

    read string

--- scripts/utils.py
+++ scripts/utils.py
@@ -21,7 +21,7 @@ def get_env_config(CONFIG_FILES_DIR):
     if os.path.isfile(CONFIG_FILES_DIR + "/config.json"):
         with open(CONFIG_FILES_DIR + "/config.json") as f:
             file = f.read()
-            env_config = json.load(file)
+            env_config = json.loads(file)
     else:
         env_config = {}
     return env_config
