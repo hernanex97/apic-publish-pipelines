4c9863c5698d896bb8490b67c1fe414a385c387b
Hernan Exposito <expositohernan@ibm.com>
2024-02-21 17:06:24 -0300

    source

--- scripts/apic_platform_publish_to_catalog.py
+++ scripts/apic_platform_publish_to_catalog.py
@@ -18,8 +18,8 @@ def get_api_name_from_product(env_local_target_dir, product_file_name):
     
     var_apilist = []
     try:
-        print("Current working directory:", os.getcwd())        # with open(env_local_target_dir + "/" + product_file_name) as f:
-        with open("/source/config/" + product_file_name) as f:
+        # with open(env_local_target_dir + "/" + product_file_name) as f:
+        with open("/source/" + product_file_name) as f:
             print("printing F in open")
             print(f)
             # use safe_load instead load
